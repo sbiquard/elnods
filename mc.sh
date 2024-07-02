8c33866d56aed738f46c8ad2bf325ae9abc2bd7e
Simon Biquard <sbiquard+git@gmail.com>
2024-07-02 16:29:50 +0200

    fix shell script

--- mc.sh
+++ mc.sh
@@ -5,18 +5,19 @@ stop=$2
 
 logfile="elnod_out/mc_${start}_${stop}.log"
 if [[ -e $logfile ]]; then
-	echo "$logfile exists. Overwriting!"
+    echo "$logfile exists. Overwriting!"
 else
     echo "Writing to $logfile"
     touch $logfile
 fi
 
-echo "Running MC from $start to $stop" > $logfile
+echo "Running MC from $start to $stop" >$logfile
 
 for ((real = $start; real < $stop; real++)); do
-    echo "--------------------" >> $logfile
-    echo "real = $real" >> $logfile
-    python3 gains.py --real $real >> $logfile 2>>&1
+    echo "" >>$logfile
+    echo "----------------------------------------" >>$logfile
+    echo "real = $real" >>$logfile
+    python3 gains.py --real $real >>$logfile 2>&1
 done
 
 echo "Done! Check $logfile for details."
