6babfdbc627e454504c447e9014fc9439ad1f26d
Simon Biquard <sbiquard+git@gmail.com>
2024-07-03 12:04:38 +0200

    add setup script to generate focalplanes and schedule

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# script to set up the simulations
+
+# get focalplanes for SAT1
+# requires s4sim (https://github.com/CMB-S4/s4sim) and toast
+mkdir -p focalplanes
+cd focalplanes
+s4_hardware_to_toast3.py --telescope SAT1 --by-tube
+
+# extract a CES from a sample schedule
+cd ../schedules
+schedule=atacama.1ces.txt
+head -n 4 sample.txt >$schedule
